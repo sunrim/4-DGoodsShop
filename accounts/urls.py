80b7ea509f38fc9ce518882397979179cf53c377
tjgpfla020 <tjgpfla>
2020-09-17 09:39:55 +0900

    accounts/urls register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from accounts import views
+
 urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+  path('register/', views.register, name='register'),
 ]
