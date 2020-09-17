e7dcf0cf28e6fa0d983f98ac94d3c178b5d3cb61
tjgpfla020 <tjgpfla>
2020-09-17 10:15:11 +0900

    startapp cart

--- DGoodsShop/settings.py
+++ DGoodsShop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop',
     'accounts',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +125,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
-MDEIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
