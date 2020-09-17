efbee9dacd51cb90e0551bc096815be9ee6b186d
tjgpfla020 <tjgpfla>
2020-09-17 09:39:04 +0900

    accounts/views register

--- accounts/views.py
+++ accounts/views.py
@@ -4,16 +4,16 @@ from accounts.forms import RegisterForm
 
 
 def register(request):
-    if request.method == 'POST':
-        user_form = RegisterForm(request.POST)
-        if user_form.is_valid():
-            new_user:user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.save()
+  if request.method == 'POST':
+    #회원가입 데이터 입력하고 버튼 눌렀을 때
+    user_form = RegisterForm(request.POST)
+    if user_form.is_valid():
+      new_user = user_form.save(commit=False)
+      new_user.set_password(user_form.cleaned_data['password'])
+      new_user.save()
 
-        return render(request, 'accounts/register')
-    else:
-        user_form=RegisterForm()
+    return render(request, 'accounts/register_done.html', {'new_user': new_user})
+  else: #회원가입 폼 나오게
+    user_form = RegisterForm()
 
-
-    return render(request, 'accounts/register.html', {'form':user_form})
+  return render(request, 'accounts/register.html', {'form': user_form})
