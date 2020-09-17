cd2d7100659812588c43316e08ec8645f18c9730
tjgpfla020 <tjgpfla>
2020-09-17 09:37:13 +0900

    accounts/forms RegisterForm

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+  password = forms.CharField(label='패스워드', widget=forms.PasswordInput)
+  password2 = forms.CharField(label='패스워드 확인', widget=forms.PasswordInput)
+
+  class Meta:
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+
+  def clean_password2(self):
+    cd = self.cleaned_data
+    if cd['password'] != cd['password2']:
+      raise forms.ValidationError('패스워드가 같아야합니다.')
+
+    return cd['password2']
