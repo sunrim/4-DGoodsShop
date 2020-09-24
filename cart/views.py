c354e2fe37b9ffe9b5c36a8685a2d37de0b2093a
tjgpfla020 <tjgpfla>
2020-09-24 09:37:48 +0900

    cart/views add, remove, detail

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from cart.cart import Cart
+from cart.forms import AddProductForm
+from shop.models import Product
+
+
+@require_POST
+def add(request, product_id):
+  cart = Cart(request)
+  product = get_object_or_404(Product, id=product_id)
+
+  form = AddProductForm(request.POST)
+  if form.is_valid():
+    cd = form.cleaned_data
+    cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
+
+  return redirect('cart:detail')
+
+
+def remove(request, product_id):
+  cart = Cart(request)
+  product = get_object_or_404(Product, id=product_id)
+  cart.remove(product)
+
+  return redirect('cart:detail')
+
+
+def detail(request):
+  cart = Cart(request)
+  for product in cart:
+    product['quantity_form'] = AddProductForm(initial={'quantity': product['quantity'], 'is_update': True})
+  return render(request, 'cart/detail.html', {'cart': cart})
