f10a161e0fe5e72e620c24995e9798b3e861f20d
tjgpfla020 <tjgpfla>
2020-09-24 09:41:32 +0900

    shop/views add_to_cart in product_detail

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
+from cart.cart import Cart
+from cart.forms import AddProductForm
 from shop.models import Product, Category
 
 
@@ -7,13 +9,15 @@ def product_in_category(request, category_slug=None):
     current_category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available_display=True)
+    cart = Cart(request)
 
     if category_slug:
         current_category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=current_category)
 
-    return render(request, 'shop/list.html', {'categories': categories, 'products':products})
+    return render(request, 'shop/list.html', {'categories': categories, 'products': products, 'cart': cart})
 
 def product_detail(request, id, product_slug):
     product = get_object_or_404(Product, id=id, slug=product_slug)
-    return render(request, 'shop/detail.html',{'product':product})
+    add_to_cart = AddProductForm(initial={'quantity': 1})
+    return render(request, 'shop/detail.html', {'product': product, 'add_to_cart': add_to_cart})
