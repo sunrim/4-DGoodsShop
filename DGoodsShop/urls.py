44a180aee255ac45fd0310078031cac282b27f47
tjgpfla020 <tjgpfla>
2020-09-24 09:38:51 +0900

    urls, cart/urls detail, product_add, product_remove

--- DGoodsShop/urls.py
+++ DGoodsShop/urls.py
@@ -20,8 +20,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    path('', include('shop.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ cart/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from cart import views
+
+app_name = 'cart'
+
+urlpatterns = [
+  path('', views.detail, name='detail'),
+  path('add/<int:product_id>/', views.add, name='product_add'),
+  path('remove/<int:product_id>/', views.remove, name='product_remove'),
+]
